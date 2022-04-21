a398b367e972966d0854c16c15185ae11fa596ca
zerex1502 <zerex1502@gmail.com>
2022-04-20 19:14:23 -0500

    ArrayList: Update

--- src/co/com/cesde/Main.java
+++ src/co/com/cesde/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        Scanner inputData = new Scanner(System.in);
+        Scanner inputData = new Scanner(System.in);
         ArrayList<Integer> data = new ArrayList<>();
 
-        /*System.out.println("Digite un valor numerico");
-        data.add(inputData.nextInt());*/
+        System.out.println("Digite un valor numerico");
+        data.add(inputData.nextInt());
         data.add(8);
         data.add(1, 9);
         data.add(2);
 
         data.add(3, 7);
-       /* System.out.println("Valor del array en la " + 3 + " = " + data.get(3));*/
+        System.out.println("Valor del array en la " + 3 + " = " + data.get(3));
         data.set(3, 12);
 
         for (int dat : data) {
